cb0662b46cfb1e4071dc650c442fcb1d18d32baf
chandrakant kaski <kaskicandrakant@gmail.com>
2018-01-08 15:37:46 +0530

    new pull req. with '#' will be ignored, and an empty message aborts the commit.

--- src/strictParser.js
+++ src/strictParser.js
@@ -1,9 +1,13 @@
 const Parser=require("./keyValueParser.js");
 const strictParseInfoCreator=require("./parseInfoCreator.js").strict;
 
-var StrictParser=function(listOfKeys) {
+var StrictParser=function(listOfKeys,ignoreCase=true) {
   Parser.call(this);
   let sanitisedListOfKeys=listOfKeys||[];
+  if(ignoreCase){
+    sanitisedListOfKeys=listOfKeys.map((ele)=>ele.toLowerCase())||[];
+    console.log(sanitisedListOfKeys);
+  }
   this.parseInfoCreator=strictParseInfoCreator(sanitisedListOfKeys);
 }
 
